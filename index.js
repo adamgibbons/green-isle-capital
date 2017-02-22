f85d8e711dd41d40cbffcdba98eef1130bca2e10
adamgibbons <adam.d.gibbons@gmail.com>
2017-02-22 11:37:30 -0700

    adds watching/hot reload

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
 var permalinks  = require('metalsmith-permalinks');
 var less        = require('metalsmith-less');
+var watch       = require('metalsmith-watch');
 
 Metalsmith(__dirname)
-  .metadata({
-    title: "My Static Site & Blog",
-    description: "It's about saying »Hello« to the World.",
-    generator: "Metalsmith",
-    url: "http://www.metalsmith.io/"
-  })
+  // .metadata({
+  //   title: "My Static Site & Blog",
+  //   description: "It's about saying »Hello« to the World.",
+  //   generator: "Metalsmith",
+  //   url: "http://www.metalsmith.io/"
+  // })
   .source('./src')
   .destination('./build')
   .clean(false)
@@ -20,6 +21,15 @@ Metalsmith(__dirname)
   .use(layouts({
     engine: 'pug'
   }))
+  .use(
+    watch({
+      paths: {
+       "${source}/**/*": true,
+        "layouts/**/*": "**/*.md",
+      },
+      livereload: true
+    })
+  )
   .build(function(err, files) {
     if (err) { throw err; }
   });
